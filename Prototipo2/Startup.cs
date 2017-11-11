8c60ffd194ace2229fda047189ac4e70af543203
Administrador <Administrador@GGIECLB2WRK134>
2017-11-11 15:31:20 -0500

    Agregar archivos de proyecto.

---
+++ Prototipo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prototipo2.Startup))]
+namespace Prototipo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
